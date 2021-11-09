18fa551401454644cb62c2134847d09b61c6974b
patrickgilsf <johnpatrickgilligan@gmail.com>
2021-11-08 17:04:17 -0800

    end of day push from progress on second module

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-
-
-
-
+absolutePath = __dirname + "/public/index.html";
+app.get('/',function(req,res) {
+    res.send("Hello Express");
+    res.sendFile(absolutePath);
+})
+console.log("Hello World");
 
 
 
